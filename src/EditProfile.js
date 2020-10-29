5cfe7ee1ffefa7819ffb968c6e5924868ad35067
hiwotg <hiwotg734@gmail.com>
2020-10-28 23:25:33 -0400

    Update on Ask page and Edit Profile

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
-import { View, Text, ScrollView, TextInput } from "react-native";
+import { View, Text, ScrollView, TextInput, Stylesheet } from "react-native";
 import { Header, Avatar, Input, Button } from "react-native-elements";
+import { Picker } from "@react-native-community/picker";
+import DropDownPicker from "react-native-dropdown-picker";
+//import Icon from "react-native-vector-icons/Feather";
+
+//import RNPickerSelect from "react-native-picker-select";
 class EditProfile extends Component {
+  state = {
+    gender: "female",
+    language: "amharic",
+  };
   render() {
     return (
       <View style={{ flex: 1 }}>
@@ -58,7 +67,122 @@ class EditProfile extends Component {
               }}
               placeholder="Update bio..."
             />
-            <Button title="Update bio" type="outline" />
+            <Button style={{ marginLeft: 200 }} title="Update bio" />
+          </View>
+
+          <View
+            style={{
+              marginVertical: 10,
+            }}
+          >
+            <TextInput
+              style={{
+                borderWidth: 0.25,
+                minHeight: 50,
+                borderRadius: 4,
+                padding: 10,
+                fontSize: 15,
+                backgroundColor: "white",
+                borderColor: "gray",
+                marginBottom: 10,
+              }}
+              placeholder="First Name"
+            />
+            <TextInput
+              style={{
+                borderWidth: 0.25,
+                minHeight: 50,
+                borderRadius: 4,
+                padding: 10,
+                fontSize: 15,
+                backgroundColor: "white",
+                borderColor: "gray",
+                marginBottom: 10,
+              }}
+              placeholder=" Last Name"
+            />
+
+            <View
+              style={{
+                marginTop: 15,
+                borderWidth: 0.25,
+                minHeight: 50,
+                borderRadius: 4,
+                padding: 10,
+                fontSize: 15,
+                backgroundColor: "white",
+                borderColor: "gray",
+                marginBottom: 10,
+              }}
+            >
+              <DropDownPicker
+                items={[
+                  {
+                    label: "AMHARIC",
+                    value: "amharic",
+                    hidden: true,
+                  },
+                  {
+                    label: "ENGLISH",
+                    value: "english",
+                  },
+                ]}
+                defaultValue={this.state.any}
+                containerStyle={{ height: 40 }}
+                style={{ backgroundColor: "#fafafa" }}
+                itemStyle={{
+                  justifyContent: "flex-start",
+                }}
+                dropDownStyle={{ backgroundColor: "#fafafa" }}
+                onChangeItem={(item) =>
+                  this.setState({
+                    language: item.value,
+                  })
+                }
+              />
+            </View>
+            <View
+              style={{
+                marginTop: 15,
+                borderWidth: 0.25,
+                minHeight: 50,
+                borderRadius: 4,
+                padding: 10,
+                fontSize: 15,
+                backgroundColor: "white",
+                borderColor: "gray",
+                marginBottom: 10,
+              }}
+            >
+              <DropDownPicker
+                style={{ marginTop: 50 }}
+                items={[
+                  {
+                    label: "FEMAL",
+                    value: "female",
+                    hidden: true,
+                  },
+                  {
+                    label: "MALE",
+                    value: "male",
+                  },
+                ]}
+                defaultValue={this.state.any}
+                containerStyle={{ height: 40 }}
+                style={{ backgroundColor: "#fafafa" }}
+                itemStyle={{
+                  justifyContent: "flex-start",
+                }}
+                dropDownStyle={{ backgroundColor: "#fafafa" }}
+                onChangeItem={(item) =>
+                  this.setState({
+                    gender: item.value,
+                  })
+                }
+              />
+            </View>
+
+            <Button style={{ marginTop: 40 }} title="Update info" />
           </View>
         </ScrollView>
       </View>
